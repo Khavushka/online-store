4556c00946a23ca35a5d22b88c7fdb4e5b968803
Khavushka <khavushka@gmail.com>
2021-12-10 16:33:06 +0100

    lokale ændringer

--- app.js
+++ app.js
@@ -14,7 +14,34 @@ app.use(express.static('public'));
 app.set('view engine', 'pug');
 
 app.get('/cool', (req, res) => res.send(cool()))
-
+app.get('/times', (req, res) => res.send(showTimes()))
+showTimes = () => {
+  let result = '';
+  const times = process.env.TIMES || 5;
+  for (i = 0; i < times; i++) {
+    result += i + ' ';
+  }
+  return result;
+}
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 /**
 * opsætning mysql
 */
